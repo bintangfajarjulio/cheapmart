5da5afb47c2e3e298b38ade289722de04cecd6ac
bintangfajarjulio <bintangin14@gmail.com>
2022-01-20 21:25:42 +0700

    initial commit

---
+++ style.js
@@ -0,0 +1,41 @@
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+});
+
+/*kasir
+function perkalian(kuantitas, harga){
+	bayar = kuantitas * harga;
+	return bayar;
+}
+
+function masako(){
+	var beli = prompt("Jumlah Pembelian","1");
+	beli = parseInt(beli);
+	var harga = 1500;
+	var hasilperkalian = perkalian(beli, harga);
+	document.form1.total.value = hasilperkalian;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
